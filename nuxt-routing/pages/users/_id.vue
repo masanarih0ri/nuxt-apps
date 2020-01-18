148608e2345cd59d85ce12115b899b1b97b13598
masanarih0ri <masanarihoridevelop@gmail.com>
2020-01-18 18:00:25 +0900

    nuxt-routingのフォルダを追加

---
+++ nuxt-routing/pages/users/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <p>{{ message }}</p>
+    <p>id: {{ this.$route.params.id }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: 'uses/_id.vueを表示中'
+    }
+  },
+  validate({ params }) {
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ nuxt-routing/pages/users/other.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>users/other.vue</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
