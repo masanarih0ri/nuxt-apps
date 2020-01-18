089ccb2cce6bccf94edd5b7f82afb77939db55a4
masanarih0ri <masanarihoridevelop@gmail.com>
2020-01-18 19:57:13 +0900

    nuxt-asyncを追加

---
+++ nuxt-async/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+      <!-- {{users[0].id}}, {{users[0].name}} -->
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{user.id}}, {{user.name}}, {{user.company.name}}
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+let url = 'https://jsonplaceholder.typicode.com/useddddrs';
+
+
+export default {
+  asyncData({ params, error }) {
+    return axios.get(url)
+    .then((res) => {
+      return { users: res.data }
+    })
+    .catch((e) => {
+      // console.log(error.response.status)
+      error({ users: e.response.status, message: e.message })
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
