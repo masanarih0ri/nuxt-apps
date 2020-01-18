f677f2eb5dccccad4ba0ff6bd4026fecfee325f3
masanarih0ri <masanarihoridevelop@gmail.com>
2020-01-18 17:20:57 +0900

    ディレクトリ 構成を変更

---
+++ nuxt_first_exercise/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div>
+      Top page
+    </div>
+    <hr>
+    <router-link to="/mypage">mypage</router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ nuxt_first_exercise/pages/mypage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div>
+      My page
+    </div>
+    <hr>
+    <router-link to="/">toppage</router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
