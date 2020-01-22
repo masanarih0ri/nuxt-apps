1d6757818506ab1d45c5d5fb3c28aea9ad4559a5
masanarih0ri <masanarihoridevelop@gmail.com>
2020-01-23 08:59:12 +0900

    nuxt-vuexを追加

---
+++ nuxt-vuex/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <!-- <p>{{ $store.state.message }}</p> -->
+    <p>{{ $store.state.hello.message }}</p>
+    <!-- <button @click="$store.commit('updateMessage')">update</button> -->
+    <!-- <button @click="$store.commit('updateMessage', 'commit with payload')">update</button> -->
+    <!-- <button @click="$store.dispatch('updateMessageAction')">Dispatch</button> -->
+    <!-- <button @click="$store.dispatch('updateMessageAction', 'Dispatch with payload')">Dispatch</button> -->
+    <button @click="$store.dispatch('hello/updateMessageAction', 'Dispatch with payload')">Dispatch</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
